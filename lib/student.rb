83fbdb13bf9df4ad4b311c626b4a1ba81991fd96
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-19 11:03:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -95,7 +95,7 @@ class Student
         LIMIT 1
         SQL
     
-    DB[:conn].execute(sql, x).map{|row| Student.new_from_db(row)}
+    DB[:conn].execute(sql).map{|row| Student.new_from_db(row)}.first
   end
   
     
