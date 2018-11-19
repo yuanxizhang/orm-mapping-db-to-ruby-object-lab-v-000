ca6097ece0b6ca6964b3c4072d212b643ccc601c
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-19 11:00:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,5 +77,13 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(x)
+  sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade = 10 
+        LIMIT ?
+        SQL
+    
+    DB[:conn].execute(sql, x).map{|row| Student.new_from_db(row)}
+  end
     
 end
